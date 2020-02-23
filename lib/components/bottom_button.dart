c44fb61d2ab561d4001a4d1351d863795e360d3a
lokeshs1010 <lokesh1010saini@gmail.com>
2020-02-23 17:07:34 +0530

    feat: 🎸 Implement bmi calculator

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class BottomButton extends StatelessWidget {
+  final Function onTap;
+  final String buttonTitle;
+
+  BottomButton({@required this.buttonTitle, @required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColor,
+        width: double.infinity,
+        padding: EdgeInsets.only(bottom: 20.0),
+        height: kBottomContainerHeight,
+        margin: EdgeInsets.only(top: 10.0),
+      ),
+    );
+  }
+}
